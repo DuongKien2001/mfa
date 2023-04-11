3fa174ca2985428ea219a06254289b6f3e020ad5
DuongKien2001 <trungkienty2001@gmail.com>
2023-04-11 17:26:10 +0700

    k

--- train.py
+++ train.py
@@ -58,10 +58,10 @@ def train(gpu, args, cfg):
     if rank == 0:
         logger.info(type(model_A))
         logger.info(trainer)
-    for e in range(cfg.SOLVER.MAX_EPOCHS-54):
+    for e in range(cfg.SOLVER.MAX_EPOCHS-43):
 
-        source_train_sampler.set_epoch(e-54)
-        target_train_sampler.set_epoch(e-54)
+        source_train_sampler.set_epoch(e+43)
+        target_train_sampler.set_epoch(e+43)
 
         for batch in (tqdm(trainer.tt_dl) if rank==0 else trainer.tt_dl):
             trainer.step(batch)
