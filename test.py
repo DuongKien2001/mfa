c448d8014c677a822b4b6605e5d00765048dc33c
DuongKien2001 <trungkienty2001@gmail.com>
2023-04-11 11:08:20 +0700

    k

--- test.py
+++ test.py
@@ -132,5 +132,5 @@ if __name__ == '__main__':
     if not os.path.exists(args.output):
         os.mkdir(args.output)
     _, _, target_val_loader = make_dataloader(cfg, 0)
-    for i in range(9):
-        test_model(i+10, target_val_loader, args, cfg, gpu=args.gpu)
+    for i in range(19):
+        test_model(i+1, target_val_loader, args, cfg, gpu=args.gpu)
